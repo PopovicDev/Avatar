e3c3f9601aa6da08ffc5fc242d5cb612349af986
PopovicDev <103214759+PopovicDev@users.noreply.github.com>
2022-12-03 03:54:54 +0100

    Updated

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+let main_page = document.getElementById('page');
+let loading_screen = document.getElementById('loading-page');
+
+setTimeout(() => {
+	main_page.style.display = 'block';
+	loading_screen.style.display = 'none';
+}, 8500);
