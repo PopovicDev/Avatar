9a04d9e6354fb95c85e17cebcd0d76d86709f581
PopovicDev <103214759+PopovicDev@users.noreply.github.com>
2022-12-03 03:52:31 +0100

    Delete.

--- javascript/index.js
+++
@@ -1,7 +0,0 @@
-let main_page = document.getElementById('page');
-let loading_screen = document.getElementById('loading-page');
-
-setTimeout(() => {
-	main_page.style.display = 'block';
-	loading_screen.style.display = 'none';
-}, 8500);
