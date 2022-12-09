21b84750a7297b621c3ea16486d226bf3f8a6fd8
PopovicDev <103214759+PopovicDev@users.noreply.github.com>
2022-12-09 15:25:58 +0100

    Updated.

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,14 @@
-let main_page = document.getElementById('page');
+let main_page = document.getElementById('main-page');
 let loading_screen = document.getElementById('loading-page');
 
 setTimeout(() => {
 	main_page.style.display = 'block';
 	loading_screen.style.display = 'none';
 }, 8500);
+
+window.onscroll = function(){
+	var picture = document.getElementById('katara');
+	if(window.pageYOffset > 500){
+		picture.classList.add('katara-on');
+	}
+}
