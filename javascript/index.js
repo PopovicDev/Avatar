2e73172ec43648df856ac6e7fac6ecf7a0986244
PopovicDev <103214759+PopovicDev@users.noreply.github.com>
2023-01-16 12:07:58 +0100

    Header added.

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,12 @@
 let main_page = document.getElementById('main-page');
 let loading_screen = document.getElementById('loading-page');
+let header_page = document.getElementById('header');
 
 setTimeout(() => {
 	main_page.style.display = 'block';
+	header_page.style.display = 'flex';
 	loading_screen.style.display = 'none';
-}, 8500);
+}, 8300);
 
 window.onscroll = function(){
 	var picture = document.getElementById('katara');
