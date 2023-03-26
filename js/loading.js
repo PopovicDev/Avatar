0fa9ea3ef3cb93f00dc3999dd835a341823e80ea
Milan Popović <poppovic.contact@gmail.com>
2023-03-26 14:44:39 +0200

    Working.

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,17 @@
-let main_page = document.getElementById('main-page');
-let loading_screen = document.getElementById('loading-page');
-let header_page = document.getElementById('header');
+var loader = document.querySelector("#loading-page");
+var main_page = document.getElementById('main-page');
+var header = document.getElementById('header');
 
-setTimeout(() => {
-	main_page.style.display = 'block';
-	header_page.style.display = 'flex';
-	loading_screen.style.display = 'none';
-}, 7000);
+window.addEventListener('load', vanish_and_add);
+
+function vanish_and_add(){
+	setTimeout(() => {
+		loader.classList.add("disappear");
+		main_page.style.display = 'block';
+		header.style.display = 'flex';
+	}, 5000);
+
+	setTimeout(() => {
+		loader.style.display = 'none';
+	}, 6500);
+}
