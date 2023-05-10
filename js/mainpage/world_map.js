3c350272a93487c37afd643f377e7c194a2b2eec
Milan Popović <poppovic.contact@gmail.com>
2023-05-10 23:56:20 +0200

    Code optimization [world_map.js]

--- js/mainpage/world_map.js
+++ js/mainpage/world_map.js
@@ -1,80 +1,23 @@
 let picture = document.getElementById('picture');
-let texts = [
-    document.getElementById('AirOne'),
-    document.getElementById('AirTwo'),
-    document.getElementById('AirThree'),
-    document.getElementById('WaterOne'),
-    document.getElementById('WaterTwo'),
-    document.getElementById('Earth'),
-    document.getElementById('Fire')
-]
-let checks = [
-    document.getElementById('check1'),
-    document.getElementById('check2'),
-    document.getElementById('check3'),
-    document.getElementById('check4'),
-    document.getElementById('check5'),
-    document.getElementById('check6'),
-    document.getElementById('check7'),
-    document.getElementById('check8'),
-    document.getElementById('check9')
-]
-function Vazduh1(){
-    picture.src="./pictures/world_map/air1-world-map.webp";
+let textz = picture.closest("#world-map-map").querySelectorAll('[data-texts]');
+let texts = [...textz];
+function Picture(value){
+    picture.src = "./pictures/world_map/" + value;
 }
-function Vazduh2(){
-    picture.src="./pictures/world_map/air2-world-map.webp";
+function Territory(textnum, value){
+    parseInt(textnum);
+    for(i in texts){
+        texts[i].style.display = 'none';
+    }
+    picture.src="./pictures/world_map/default-world-map.webp";
+    texts[textnum].style.display = 'block';
+    Picture(value);
 }
-function Vazduh3(){
-    picture.src="./pictures/world_map/air3-world-map.webp";
-}
-function Water1(){
-    picture.src="./pictures/world_map/water1-world-map.webp";
-}
-function Water2(){
-    picture.src="./pictures/world_map/water2-world-map.webp";
-}
-function Earth(){
-    picture.src="./pictures/world_map/earth-world-map.webp";
-}
-function Fire(){
-    picture.src="./pictures/world_map/fire-world-map.webp";
-}
-window.addEventListener('click', function(e){
-    switch(true){
-        case (checks[0].contains(e.target) || texts[0].contains(e.target)):
-            texts[0].style.display = 'block';
-            Vazduh1();
-            break;
-        case (checks[1].contains(e.target) || texts[1].contains(e.target)):
-            texts[1].style.display = 'block';
-            Vazduh2();
-            break;
-        case (checks[2].contains(e.target) || texts[2].contains(e.target)):
-            texts[2].style.display = 'block';
-            Vazduh3();
-            break;
-        case (checks[3].contains(e.target) || texts[3].contains(e.target)):
-            texts[3].style.display = 'block';
-            Water1();
-            break;
-        case (checks[4].contains(e.target) || texts[4].contains(e.target)):
-            texts[4].style.display = 'block';
-            Water2();
-            break;
-        case (checks[5].contains(e.target) || checks[6].contains(e.target) || checks[7].contains(e.target) || texts[5].contains(e.target)):
-            texts[5].style.display = 'block';
-            Earth();
-            break;
-        case (checks[8].contains(e.target) || texts[6].contains(e.target)):
-            texts[6].style.display = 'block';
-            Fire();
-            break;
-        default:
-            for(let i = 0; i < texts.length; i++){
-                texts[i].style.display = 'none';
-            }
-            picture.src="./pictures/world_map/default-world-map.webp";
-            break;
+window.addEventListener('mouseup', (e) => {
+    if(!picture.contains(e.target)){
+        for(let i = 0; i < texts.length; i++){
+            texts[i].style.display = 'none';
+        }
+        picture.src="./pictures/world_map/default-world-map.webp";
     }
 });
